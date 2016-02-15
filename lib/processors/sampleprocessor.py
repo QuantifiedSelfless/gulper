90fa5d767a82ff3c1a2ce62b3695581490751e04
Micha Gorelick <mynameisfiber@gmail.com>
2016-02-14 20:33:03 -0700

    some comments

--- lib/processors/sampleprocessor.py
+++ lib/processors/sampleprocessor.py
@@ -8,6 +8,10 @@ class SampleProcessor(object):
 
     @gen.coroutine
     def process(self, user_data):
+        """
+        Process the scraped data inside of user_data and save it locally.  It
+        can save it to file, or a database... no one really cares
+        """
         print("Processing user: ", user_data.userid)
         userid = user_data.userid
         self.data[userid] = sum(len(i['text']) for i in user_data.data['text'])
@@ -16,10 +20,17 @@ class SampleProcessor(object):
 
     @gen.coroutine
     def num_characters(self, userid, request, public_key=None):
+        """
+        Returns relevant data that the exhibits may want to know
+        """
         return self.data.get(userid, 'userid not found')
 
     @process_api_handler
     def register_handlers(self):
+        """
+        Registers any http handlers that this processor wants to have availible
+        to exhibits
+        """
         return [
             ('num_characters', self.num_characters),
         ]
